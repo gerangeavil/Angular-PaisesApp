59a385add29f09babf19227d1c121552c4366ad0
Gerson Angel <gerson.angel@ab-inbev.com>
2022-02-05 16:26:10 -0500

    DebounceTime - en el input

--- src/app/paises/components/paises-input/paises-input.component.ts
+++ src/app/paises/components/paises-input/paises-input.component.ts
@@ -1,14 +1,31 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-paises-input',
   templateUrl: './paises-input.component.html'
 })
-export class PaisesInputComponent {
-  @Output() onEnter: EventEmitter<string> = new EventEmitter;
+export class PaisesInputComponent implements OnInit {
+  @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+  debouncer: Subject<string> = new Subject();
   termino: string = '';
+
+  ngOnInit() {
+    this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe(valor => {
+        this.onDebounce.emit(valor);
+      });
+  }
 
   buscar() {
     this.onEnter.emit(this.termino);
+  }
+
+  teclaPresionada() {
+    this.debouncer.next(this.termino);
+
   }
 }
--- src/app/paises/pages/por-pais/por-pais.component.ts
+++ src/app/paises/pages/por-pais/por-pais.component.ts
@@ -29,4 +29,8 @@ export class PorPaisComponent {
       this.paises = [];
     });
   }
+
+  sugerencias(termino: string) {
+    this.hayError = false;
+  }
 }
