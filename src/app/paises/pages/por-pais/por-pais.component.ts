b14036602da7e7629dd074161c92a7ef42da1167
Gerson Angel <gerson.angel@ab-inbev.com>
2022-05-08 21:31:08 -0500

    Mostrar sugerencias al escribir - autocomplete

--- src/app/paises/pages/por-pais/por-pais.component.ts
+++ src/app/paises/pages/por-pais/por-pais.component.ts
@@ -6,21 +6,29 @@ import { Country } from '../../interfaces/paises.interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(
     private paisesService: PaisesService
   ) { }
 
   buscar(termino: string) {
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
-    this.paisesService.buscarPais(termino).subscribe((paises) => {
+    this.paisesService.buscarPais(termino).subscribe(paises => {
       this.paises = paises;
       console.log(paises);
     }, error => {
@@ -32,5 +40,16 @@ export class PorPaisComponent {
 
   sugerencias(termino: string) {
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisesService.buscarPais(termino)
+      .subscribe(paises => this.paisesSugeridos = paises.splice(0, 5),
+        error => this.paisesSugeridos = []
+      );
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
   }
 }
